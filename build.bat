704e6c661d48c588cc6fd59111a9f4a8e1c9df70
Topher Brown <chris.brown@farsounder.com>
2014-11-23 22:20:25 -0500

    make batch file one line

--- build.bat
+++ build.bat
@@ -6,10 +6,4 @@ for /F "tokens=*" %%I in (%~dp0settings.ini) do set %%I
 
 :: Remove the 'write completed' part when coffee-stir fixes issue #2
 set datetime=%date:~-10,2%%date:~-7,2%%date:~-4,4%_%time:~0,2%%time:~3,2%%time:~6,2%
-coffee-stir src/main.coffee |  ^
-    sed 's/write completed//' |  ^
-    coffee --compile --stdio |  ^
-    sed -e 's/insert_version_here/%datetime%/'  ^
-    sed -e 's/insert_mode_here/%mode%/' |  ^
-    tee generated_bundle.js |  ^
-    clip
+coffee-stir src/main.coffee | sed 's/write completed//' | coffee --compile --stdio | sed -e 's/insert_mode_here/%mode%/' -e 's/insert_version_here/%datetime%/' | tee generated_bundle.js | clip
